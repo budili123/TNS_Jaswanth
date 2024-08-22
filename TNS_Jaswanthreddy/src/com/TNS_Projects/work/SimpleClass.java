a8efb58b81b499cc6b7fa14cfb1b2c54cf7cdf7e
jaswanth reddy <jaswanthreddy2004b@gmail.com>
2024-08-22 21:28:45 +0530

    commit 3

--- TNS_Jaswanthreddy/src/com/TNS_Projects/work/SimpleClass.java
+++ TNS_Jaswanthreddy/src/com/TNS_Projects/work/SimpleClass.java
@@ -3,6 +3,7 @@ package com.TNS_Projects.work;
 public class SimpleClass {
 
 	public static void main(String[] args) {
+		System.out.println("jaswanth");
 		
 		System.out.println("welcom to github");
 	}
