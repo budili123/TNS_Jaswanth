8669db43d1f31abcd55536a9caa7aa82d379760a
jaswanth reddy <jaswanthreddy2004b@gmail.com>
2024-08-22 21:24:19 +0530

    commit 2

--- TNS_Jaswanthreddy/src/com/TNS_Projects/work/SimpleClass.java
+++ TNS_Jaswanthreddy/src/com/TNS_Projects/work/SimpleClass.java
@@ -3,8 +3,8 @@ package com.TNS_Projects.work;
 public class SimpleClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("welcom to github");
 	}
 
 }
---
+++ TNS_Jaswanthreddy/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TNS_Jaswanthreddy {
+}
