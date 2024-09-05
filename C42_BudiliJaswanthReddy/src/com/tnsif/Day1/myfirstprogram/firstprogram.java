404129cb1185dec11e6308dd9c35648bd8028594
jaswanth reddy <jaswanthreddy2004b@gmail.com>
2024-09-05 11:43:12 +0530

    commit1

---
+++ C42_BudiliJaswanthReddy/src/com/tnsif/Day1/myfirstprogram/firstprogram.java
@@ -0,0 +1,11 @@
+package com.tnsif.Day1.myfirstprogram;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
